297f8cc781762d62b4c50f200b3c84626f8a101d
Thy_Allan <93493237+ThyAllan@users.noreply.github.com>
2023-07-25 12:03:35 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,63 @@
+a = 100 
+b = 1250
+if a==b: 
+    print (f"{a} é igual a {b}")
+else:
+    print(f"{a} é diferente de {b}")
+    print("Os valores NÃO são iguais")
+print("Este texto será impresso.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ex1 Digita 2 numeros e imprime o maior deles 
+
+
+
+
+
+
+
+
+
+#ex2
+n = int ( input ("Digite:") )
+
+if n>0:
+    print("Positivo")
+    
+elif n ==0:
+    print("Nulo")
+
+else: 
+    print("Negativo")
+
+
+
+
+#ex3
+genero =  input("Digite F ou M:")
+
+if genero== 'F':
+    print("Feminino")
+    
+elif genero.upper== 'M':
+    print("Masculino")
+else:
+    print("Genero Inválido")
+
